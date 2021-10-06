a281bbb41ab22354ddafee62da83d0e95a4b938c
Kalpani98 <it20132828@my.sliit.lk>
2021-10-06 13:54:14 +0530

    commited

---
+++ BACKEND/frontend/src/components/lab/Feedback.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Feedback() {
+    return (
+        <div className='body'>
+            <div className='appointment-container'>
+                <div className='title'>Feedback</div>
+                <div className='container feed'>
+                    <div>
+                        <h3>Hello</h3>
+                        <p>Hey</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
